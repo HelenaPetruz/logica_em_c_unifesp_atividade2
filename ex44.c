1f5619a14e9cf51a3ede15a16bf1df07cb1d823d
Helena <helena.petruz@gmail.com>
2026-04-12 15:45:29 -0300

    Create ex44.c

---
+++ ex44.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  float n1, n2;
+  printf("Digite o primeiro valor: ");
+  scanf("%f", &n1);
+  for(int i=1;  i>0; i++){
+    printf("Digite o segundo valor: ");
+    scanf("%f", &n2);
+  if(n2 != 0)
+    i=-1;
+  }
+  printf("%.2f dividido por %.2f é igual a %.2f", n1, n2, n1/n2);
+}
