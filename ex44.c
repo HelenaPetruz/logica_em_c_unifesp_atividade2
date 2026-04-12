de9fdfeab67d7f0362a22ed683764e1a53a0426b
Helena <helena.petruz@gmail.com>
2026-04-12 15:31:33 -0300

    Add division program with input validation

---
+++ ex44.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  float n1, n2;
+  printf("Digite o primeiro valor: ");
+  scanf("%f", &n1);
+  do{
+    printf("Digite o segundo valor: ");
+    scanf("%f", &n2);
+  } while(n2==0);
+  printf("%.2f dividido por %.2f é igual a %.2f", n1, n2, n1/n2);
+}
