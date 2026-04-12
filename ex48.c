ccb7a85d30c885b90661db38354f3e272ff65e21
Helena <helena.petruz@gmail.com>
2026-04-12 16:15:51 -0300

    Create ex48.c

---
+++ ex48.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  float n1, n2;
+  do{
+    printf("Digite a primeira nota: ");
+    scanf("%f", &n1);
+  } while(n1<0 || n1>10);
+  do{
+    printf("Digite a segunda nota: ");
+    scanf("%f", &n2);
+  } while(n2<0 || n2>10);
+  printf("Média: %.2f", (n1+n2)/2);
+}
